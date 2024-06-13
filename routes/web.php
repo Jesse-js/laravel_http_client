cae466ec8b170c64eab77a15fa02d87fb5071e4c
Jesse-js <jesse-jjs@hotmail.com>
2024-06-13 19:39:56 -0300

    chore: adds tailwindcss, autoprefixer, postcss and flowbite

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $response = Http::get('https://api.github.com/users/Jesse-js/repos');
+    
     return view('welcome');
 });
